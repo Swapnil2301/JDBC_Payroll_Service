011721e38d50131b1d80af7441d2b8f2e89a22bc
swapnil yadav <swapnilmaya19compeng@student.mes.ac.in>
2024-06-28 14:59:28 +0530

    Checking Connection and Listing Jdbc Drivers

--- src/JDBC_DB.java
+++ src/JDBC_DB.java
@@ -1,18 +1,34 @@
 import java.sql.Connection;
 import java.sql.DriverManager;
 import java.sql.SQLException;
-import java.sql.Statement;
 
 public class JDBC_DB {
-    public static void main(String[] args) throws SQLException {
+    public static void main(String[] args) {
         String url = "jdbc:mysql://localhost:3306/payroll_service";
-        String user= "root";
-        String password= "swapnil";
-        try{
-            Connection con =DriverManager.getConnection(url,user,password);
-            System.out.println("Database is Connected, Connection Info is - "+con);
-        }catch (Exception e) {
-            System.out.println(e);
+        String user = "root";
+        String password = "swapnil";
+
+        try {
+            Class.forName("com.mysql.cj.jdbc.Driver");
+            Connection con = DriverManager.getConnection(url, user, password);
+            if (con != null) {
+                System.out.println("Database connected successfully!");
+                System.out.println("Connection Info: " + con);
+                java.sql.Driver driver = DriverManager.getDriver(url);
+                System.out.println("JDBC Driver: " + driver.getClass().getName());
+                java.util.Enumeration<java.sql.Driver> driverList = DriverManager.getDrivers();
+                while (driverList.hasMoreElements()) {
+                    java.sql.Driver d = driverList.nextElement();
+                    System.out.println("Registered JDBC Driver: " + d.getClass().getName());
+                }
+
+                con.close();
+            } else {
+                System.out.println("Failed to make connection!");
+            }
+        } catch (ClassNotFoundException | SQLException e) {
+            System.out.println("Database connection failed!");
+            e.printStackTrace();
         }
     }
 }
